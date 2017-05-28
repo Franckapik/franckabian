4e2eb53d240aae86af317dfde9d01affef1fb3fb
franckapik <fanch44@hotmail.com>
2017-05-28 22:16:20 +0200

    up

--- fonction.py
+++ fonction.py
@@ -3,8 +3,8 @@ import time
 import RPi.GPIO as GPIO ## Import de la librairie obligatoire GPIO 
 
 GPIO.setmode(GPIO.BCM) ## Utilisation de la numerotation de la carte GPIO
-GPIO.setup(17, GPIO.OUT) ## Selection du pin 17
+GPIO.setup(5, GPIO.OUT) ## Selection du pin 5
 
 def op(x):
-    GPIO.output(17,x) ## Definition de la fonction generale output
+    GPIO.output(5,x) ## Definition de la fonction generale output
 
